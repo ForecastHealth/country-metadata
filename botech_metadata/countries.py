1fe03aa479baae965e963a6f84f745d592e0aeb9
Rory Watts <rory@forecasthealth.org>
2023-12-13 16:38:12 +0800

    _records pulls from a script from csv

--- botech_metadata/countries.py
+++ botech_metadata/countries.py
@@ -2,9 +2,6 @@ from typing import NamedTuple, List
 import csv
 
 
-COUNTRY_FILEPATH = "botech_metadata/countries.csv"
-
-
 class Country(NamedTuple):
     name: str
     alpha2: str
@@ -15,11 +12,13 @@ class Country(NamedTuple):
 
 def collect_country_data(fp: str) -> List[Country]:
     countries = []
-    with open(fp, newline='') as csvfile:
-        reader = csv.reader(csvfile)
+    with open(fp, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         next(reader)  # Skip the header row
         for row in reader:
             if row:  # Ensure the row is not empty
+                print(len(row))
+                print(row)
                 country = Country(*row)
                 countries.append(country)
     return countries
---
+++ scripts/metadata.py
@@ -0,0 +1,14 @@
+"""
+metadata.py
+
+Open up the class, print some stuff.
+"""
+from botech_metadata import countries
+
+
+def main():
+    print(countries.get("US"))
+
+
+if __name__ == "__main__":
+    main()
